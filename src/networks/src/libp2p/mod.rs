72cc651b351d07ce454eb49cc107d97b72332123
o.moghadas <o.moghadas@fanap.ir>
2025-02-02 22:28:45 +0330

    feat: work on netwrok implement

--- src/networks/src/libp2p/mod.rs
+++ src/networks/src/libp2p/mod.rs
@@ -1,12 +1,66 @@
 pub mod types; // Declare the `types` module
 
-use types::types::InitResult; // Import `InitResult` from the `types` module
+use std::error::Error;
+use std::time::Duration;
 
-pub struct Network {}
+use libp2p::swarm::{NetworkBehaviour, Swarm, SwarmEvent};
+use libp2p::{noise, ping, tcp, yamux, Multiaddr, SwarmBuilder};
+use types::InitResult; // Import `InitResult` from the `types` module
+
+struct MyBehaviour {
+    ping: ping::Behaviour,
+}
+
+pub struct Network {
+    swarm: Swarm<MyBehaviour>,
+}
 
 impl Network {
-    pub fn init() -> InitResult {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let behaviour = MyBehaviour {
+            ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
+        };
+
+        let swarm = SwarmBuilder::with_new_identity()
+            .with_tokio()
+            .with_tcp(
+                tcp::Config::default(),
+                noise::Config::new,
+                yamux::Config::default,
+            )?
+            .with_swarm_config(|cfg| {
+                cfg.with_idle_connection_timeout(Duration::from_secs(60)) // 60 seconds timeout
+            })
+            .with_behaviour(|_| behaviour)?
+            .build();
+
+        Ok(Network { swarm })
+    }
+
+    pub fn listen(&mut self, addr: Multiaddr) -> InitResult {
+        self.swarm.listen_on(addr)?;
         Ok(true)
+    }
+
+    pub fn dial(&mut self, addr: Multiaddr) -> InitResult {
+        self.swarm.dial(addr)?;
+        Ok(true)
+    }
+
+    pub fn run(&mut self) {
+        tokio::spawn(async move {
+            loop {
+                match self.swarm.select_next_some().await {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        println!("Listening on {}", address);
+                    }
+                    SwarmEvent::Behaviour(event) => {
+                        println!("Behaviour event: {:?}", event);
+                    }
+                    _ => {}
+                }
+            }
+        });
     }
 }
 
@@ -15,7 +69,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_server() -> Result<(), Box<dyn std::error::Error>> {
+    fn check_ping_server() -> Result<(), Box<dyn std::error::Error>> {
         let init_server = Network::init()?; // Unwrap the Result
         assert_eq!(init_server, true); // Compare the inner bool value
         Ok(()) // Return `Ok(())` for tests
