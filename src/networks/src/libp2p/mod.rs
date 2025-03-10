cbf0e9cb82bad91bff1bf6ca093bae0d1fcb6d9e
o.moghadas <o.moghadas@fanap.ir>
2025-03-10 23:28:44 +0330

    feat: work o it

--- src/networks/src/libp2p/mod.rs
+++ src/networks/src/libp2p/mod.rs
@@ -1,13 +1,18 @@
 pub use libp2p::swarm::dummy::Behaviour as DummyBehaviour;
-use libp2p::swarm::NetworkBehaviour;
 use libp2p::{
     futures::{io, StreamExt},
     gossipsub, mdns, noise, ping, quic,
-    swarm::Swarm,
+    swarm::{self, Swarm},
     tcp, yamux, Multiaddr, SwarmBuilder,
 };
 
-use std::{error::Error, time::Duration};
+use std::{
+    collections::hash_map::DefaultHasher,
+    error::Error,
+    hash::{Hash, Hasher},
+    io::ErrorKind,
+    time::Duration,
+};
 use tracing::info;
 
 #[warn(dead_code)]
@@ -16,7 +21,6 @@ pub struct Libp2pNetwork {
 }
 
 // We create a custom network behaviour that combines Gossipsub and Mdns.
-#[derive(NetworkBehaviour)]
 struct MyBehaviour {
     gossipsub: gossipsub::Behaviour,
     mdns: mdns::tokio::Behaviour,
@@ -52,31 +56,46 @@ impl Libp2pNetwork {
             .build()
     }
 
-    pub async fn create_gossip() -> Swarm<MyBehaviour> {
-        let tcp_config = tcp::Config::default();
-        let security_upgrade = noise::Config::new;
-        let multiplexer_upgrade = yamux::Config::default;
-        let gossipsub_config = gossipsub::ConfigBuilder::default()
-            .heartbeat_interval(Duration::from_secs(10))
-            .validation_mode(gossipsub::ValidationMode::Strict)
-            .build()
-            .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg));
-
-        SwarmBuilder::with_new_identity()
+    pub async fn create_gossip(
+    ) -> Result<Swarm<MyBehaviour>, Box<dyn gossipsub::ConfigBuilderError>> {
+        let mut swarm = libp2p::SwarmBuilder::with_new_identity()
             .with_tokio()
-            .with_tcp(tcp_config, security_upgrade, multiplexer_upgrade)
-            .unwrap()
+            .with_tcp(
+                tcp::Config::default(),
+                noise::Config::new,
+                yamux::Config::default,
+            )?
+            .with_quic()
             .with_behaviour(|key| {
+                // To content-address message, we can take the hash of message and use it as an ID.
+                let message_id_fn = |message: &gossipsub::Message| {
+                    let mut s = DefaultHasher::new();
+                    message.data.hash(&mut s);
+                    gossipsub::MessageId::from(s.finish().to_string())
+                };
+
+                // Set a custom gossipsub configuration
+                let gossipsub_config = gossipsub::ConfigBuilder::default()
+                    .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                    .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
+                    // signing)
+                    .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                    .build()
+                    .map_err(|msg| std::io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+                // build a gossipsub network behaviour
                 let gossipsub = gossipsub::Behaviour::new(
-                    gossipsub::MessageAuthenticity::Signed((key.clone())),
+                    gossipsub::MessageAuthenticity::Signed(key.clone()),
                     gossipsub_config,
-                );
-                let mdns =
-                    mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id());
+                )?;
+
+                let mdns = mdns::tokio::Behaviour::new(
+                    mdns::Config::default(),
+                    key.public().to_peer_id(),
+                )?;
                 Ok(MyBehaviour { gossipsub, mdns })
-            })
-            .unwrap()
-            .build()
+            })?
+            .build();
     }
 
     pub async fn run(&mut self) {
