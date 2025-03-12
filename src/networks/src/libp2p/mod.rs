daf3bfad40b74261d2e75ba6e881a9cf52855fac
o.moghadas <electromidz@gmail.com>
2025-03-12 18:15:28 +0330

    feat: work on gossip work

--- src/networks/src/libp2p/mod.rs
+++ src/networks/src/libp2p/mod.rs
@@ -32,13 +32,12 @@ pub struct GossipsubBehaviour {
     gossipsub: gossipsub::Behaviour,
     mdns: mdns::tokio::Behaviour,
 }
-
 impl MyBehaviour {
-    pub async fn crete_gossip_swap () -> Swarm<Gossip> {
+    pub async fn create_gossip_swarm() -> Result<Swarm<MyBehaviour>, Box<dyn Error>> {
         let tcp_config = tcp::Config::default();
         let security_upgrade = noise::Config::new;
-        let multiplexer_upgrade = yamux::Config::default;
-        SwarmBuilder::with_new_identity()
+        let multiplexer_upgrade = yamux::Config::default();
+        let swarm = SwarmBuilder::with_new_identity()
             .with_tokio()
             .with_tcp(tcp_config, security_upgrade, multiplexer_upgrade)
             .unwrap()
@@ -54,8 +53,7 @@ impl MyBehaviour {
                 // Set a custom gossipsub configuration
                 let gossipsub_config = gossipsub::ConfigBuilder::default()
                     .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                    .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-                    // signing)
+                    .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
                     .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
                     .build()
                     .map_err(|msg| std::io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
@@ -69,13 +67,59 @@ impl MyBehaviour {
                 let mdns = mdns::tokio::Behaviour::new(
                     mdns::Config::default(),
                     key.public().to_peer_id(),
-                ).expect("Failed to create mdns Behaviour");
+                ).expect("Failed to create mdns behaviour");
+
                 Ok(MyBehaviour { gossipsub, mdns })
             })
             .unwrap()
-            .build()
+            .build();
+
+        Ok(swarm)
     }
 }
+// impl MyBehaviour {
+//     pub async fn crete_gossip_swap () -> Swarm<Gossip> {
+//         let tcp_config = tcp::Config::default();
+//         let security_upgrade = noise::Config::new;
+//         let multiplexer_upgrade = yamux::Config::default;
+//         SwarmBuilder::with_new_identity()
+//             .with_tokio()
+//             .with_tcp(tcp_config, security_upgrade, multiplexer_upgrade)
+//             .unwrap()
+//             .with_quic()
+//             .with_behaviour(|key| {
+//                 // To content-address message, we can take the hash of message and use it as an ID.
+//                 let message_id_fn = |message: &gossipsub::Message| {
+//                     let mut s = DefaultHasher::new();
+//                     message.data.hash(&mut s);
+//                     gossipsub::MessageId::from(s.finish().to_string())
+//                 };
+//
+//                 // Set a custom gossipsub configuration
+//                 let gossipsub_config = gossipsub::ConfigBuilder::default()
+//                     .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+//                     .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
+//                     // signing)
+//                     .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+//                     .build()
+//                     .map_err(|msg| std::io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+//
+//                 // build a gossipsub network behaviour
+//                 let gossipsub = gossipsub::Behaviour::new(
+//                     gossipsub::MessageAuthenticity::Signed(key.clone()),
+//                     gossipsub_config,
+//                 )?;
+//
+//                 let mdns = mdns::tokio::Behaviour::new(
+//                     mdns::Config::default(),
+//                     key.public().to_peer_id(),
+//                 ).expect("Failed to create mdns Behaviour");
+//                 Ok(MyBehaviour { gossipsub, mdns })
+//             })
+//             .unwrap()
+//             .build()
+//     }
+// }
 
 #[warn(dead_code)]
 impl Libp2pNetwork {
