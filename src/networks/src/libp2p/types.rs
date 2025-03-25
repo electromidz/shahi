0e2da1a9cedc5d2a52a36fd89c31f5545d8d1338
o.moghadas <electromidz@gmail.com>
2025-03-25 16:35:25 +0330

    feat:complete listen network behaviour

--- src/networks/src/libp2p/types.rs
+++ src/networks/src/libp2p/types.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
+
 pub trait TP2PNetwork {
-    //async fn listen(&mut self, addr: &str) -> Result<(), Box<dyn Error>>;
-    // async fn dial(&mut self, addr: &str) -> Result<(), Box<dyn Error>>;
-    //async fn run(self);
+    async fn listen(&mut self, addr: &str) -> Result<(), Box<dyn Error>>;
+    async fn dial(&mut self, addr: &str) -> Result<(), Box<dyn Error>>;
 }
